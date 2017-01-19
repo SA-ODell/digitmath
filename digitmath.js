175072588dd9c22d28fde0499ff10e5a1c2dbb35
Steve ODell <saodell@mac.com>
2017-01-19 14:51:58 -0700

    starting point for pair programming exercise

---
+++ digitmath.js
@@ -0,0 +1,12 @@
+// Write a function that takes two numbers. For each number, you'll increase each digit by 1 except in the case of 9 where it will be turned into a 0. Then you'll add the two numbers together. For example:
+//
+// `oddAdd( 13, 50 ) >> 24 + 61 >> 85`
+// `oddAdd( 49, 16 ) >> 50 + 27 >> 77`
+//
+// Assume that you may have numbers of varying length (that is, one digit or five digits) and that they will all be positive and greater than 0.
+
+
+
+module.exports = {
+  digitmath
+};
---
+++ test/digitmathTest.js
@@ -0,0 +1,13 @@
+// const chai = require('chai');
+// const assert = chai.assert;
+// const yellPhrase = require('../yellPhrase').yellPhrase;
+
+const chai = require('chai')
+const assert = chai.assert;
+const function = require('../digitmath').digitmath;
+
+describe('digitmath', function () {
+  it('should return 34 + 56 >> 45 + 67 =  112', function() {
+    assert.equal(function( ), '112');
+  })
+})
